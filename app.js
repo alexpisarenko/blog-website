b57dbfa4f8516ddbddab37ecc6fc0fa151d82eaf
Alexander Pisarenko <alexanderpisarenko@Alexanders-MacBook-Pro.local>
2022-06-28 09:53:17 -0400

    blog website complete js express ejs

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -18,11 +19,22 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-const postArr = [];
+const post ="";
+let postArr = [];
 
 app.get("/", function (req, res) {
-  res.render("home", { homeContent: homeStartingContent });
-  console.log(postArr);
+  res.render("home", { homeContent: homeStartingContent, posts: postArr });
+});
+
+app.get("/posts/:postName", function (req, res) {
+  const post = postArr.find((post) => _.lowerCase(req.params.postName) === _.lowerCase(post.title)
+  )
+
+  if (post) {
+    res.render("post", { posts: post });
+  } else {
+    console.log("Not found");
+  }
 });
 
 app.get("/about", function (req, res) {
